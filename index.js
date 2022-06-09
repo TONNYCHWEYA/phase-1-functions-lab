ce469366175775f5cc7b9c56126a872e2c011f38
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-09 08:00:01 +0300

    commit tree 4ae8ae075b205ea27d66d0dff9a9e55cc6e9bfba

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation)
+{
+  let noOfBlock;
+ if(pickUpLocation>42){
+   noOfBlock = pickUpLocation - 42;
+
+ }else if(pickUpLocation < 42){
+   noOfBlock = 42 - pickUpLocation;
+ }
+  return noOfBlock;
+}
+function distanceFromHqInFeet(pickUpLocation) {
+  let noOfBlock = distanceFromHqInBlocks(pickUpLocation)
+  return noOfBlock * 264;
+  
+}
+function distanceTravelledInFeet(start, end){
+
+if(end > start){
+  return ((end - start) * 264);
+}else if (end < start){
+  return ((start - end) * 264);
+}
+
+}
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+  let fare;
+  if(distance <= 400){
+   return fare = 0;
+  }else if(distance >400 && distance <=2000){
+    return fare = (distance -400) * 0.02
+  }else if(distance >2000 && distance <=2500){
+    return fare = 25
+  }else if(distance >2500){
+    return 'cannot travel that far'
+  }
+  }
